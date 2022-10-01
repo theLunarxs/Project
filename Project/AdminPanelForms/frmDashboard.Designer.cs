48ea4de36cd5c24dfd42ce01b470cb3620c52a8d
Aryan <aryanesmailiea@gmail.com>
2022-10-01 20:16:49 +0330

    Finished Dashboard

--- Project/AdminPanelForms/frmDashboard.Designer.cs
+++ Project/AdminPanelForms/frmDashboard.Designer.cs
@@ -30,25 +30,33 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDashboard));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblNumTusers = new System.Windows.Forms.Label();
             this.lblUsers = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.lblNumOnlineUsers = new System.Windows.Forms.Label();
             this.lblUsersOnline = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.lblNumAdmins = new System.Windows.Forms.Label();
             this.lblAdminsOnline = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -59,24 +67,15 @@
             this.panel1.Controls.Add(this.lblUsers);
             this.panel1.Location = new System.Drawing.Point(12, 51);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(215, 169);
+            this.panel1.Size = new System.Drawing.Size(215, 205);
             this.panel1.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(79, 15);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(54, 59);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // lblNumTusers
             // 
             this.lblNumTusers.AutoSize = true;
             this.lblNumTusers.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNumTusers.ForeColor = System.Drawing.Color.White;
-            this.lblNumTusers.Location = new System.Drawing.Point(87, 114);
+            this.lblNumTusers.Location = new System.Drawing.Point(85, 137);
             this.lblNumTusers.Name = "lblNumTusers";
             this.lblNumTusers.Size = new System.Drawing.Size(33, 40);
             this.lblNumTusers.TabIndex = 1;
@@ -87,7 +86,7 @@
             this.lblUsers.AutoSize = true;
             this.lblUsers.Font = new System.Drawing.Font("Segoe UI Symbol", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsers.ForeColor = System.Drawing.Color.White;
-            this.lblUsers.Location = new System.Drawing.Point(25, 77);
+            this.lblUsers.Location = new System.Drawing.Point(33, 90);
             this.lblUsers.Name = "lblUsers";
             this.lblUsers.Size = new System.Drawing.Size(149, 35);
             this.lblUsers.TabIndex = 0;
@@ -101,24 +100,15 @@
             this.panel2.Controls.Add(this.lblUsersOnline);
             this.panel2.Location = new System.Drawing.Point(271, 51);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(215, 169);
+            this.panel2.Size = new System.Drawing.Size(215, 205);
             this.panel2.TabIndex = 0;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(76, 15);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(56, 59);
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
             // 
             // lblNumOnlineUsers
             // 
             this.lblNumOnlineUsers.AutoSize = true;
             this.lblNumOnlineUsers.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNumOnlineUsers.ForeColor = System.Drawing.Color.White;
-            this.lblNumOnlineUsers.Location = new System.Drawing.Point(88, 114);
+            this.lblNumOnlineUsers.Location = new System.Drawing.Point(87, 137);
             this.lblNumOnlineUsers.Name = "lblNumOnlineUsers";
             this.lblNumOnlineUsers.Size = new System.Drawing.Size(33, 40);
             this.lblNumOnlineUsers.TabIndex = 1;
@@ -129,7 +119,7 @@
             this.lblUsersOnline.AutoSize = true;
             this.lblUsersOnline.Font = new System.Drawing.Font("Segoe UI Symbol", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblUsersOnline.ForeColor = System.Drawing.Color.White;
-            this.lblUsersOnline.Location = new System.Drawing.Point(21, 77);
+            this.lblUsersOnline.Location = new System.Drawing.Point(21, 90);
             this.lblUsersOnline.Name = "lblUsersOnline";
             this.lblUsersOnline.Size = new System.Drawing.Size(167, 35);
             this.lblUsersOnline.TabIndex = 0;
@@ -143,24 +133,15 @@
             this.panel3.Controls.Add(this.lblAdminsOnline);
             this.panel3.Location = new System.Drawing.Point(539, 51);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(215, 169);
+            this.panel3.Size = new System.Drawing.Size(215, 205);
             this.panel3.TabIndex = 0;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(83, 15);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(53, 59);
-            this.pictureBox3.TabIndex = 0;
-            this.pictureBox3.TabStop = false;
             // 
             // lblNumAdmins
             // 
             this.lblNumAdmins.AutoSize = true;
             this.lblNumAdmins.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNumAdmins.ForeColor = System.Drawing.Color.White;
-            this.lblNumAdmins.Location = new System.Drawing.Point(93, 114);
+            this.lblNumAdmins.Location = new System.Drawing.Point(94, 137);
             this.lblNumAdmins.Name = "lblNumAdmins";
             this.lblNumAdmins.Size = new System.Drawing.Size(33, 40);
             this.lblNumAdmins.TabIndex = 1;
@@ -171,7 +152,7 @@
             this.lblAdminsOnline.AutoSize = true;
             this.lblAdminsOnline.Font = new System.Drawing.Font("Segoe UI Symbol", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAdminsOnline.ForeColor = System.Drawing.Color.White;
-            this.lblAdminsOnline.Location = new System.Drawing.Point(18, 79);
+            this.lblAdminsOnline.Location = new System.Drawing.Point(21, 90);
             this.lblAdminsOnline.Name = "lblAdminsOnline";
             this.lblAdminsOnline.Size = new System.Drawing.Size(191, 35);
             this.lblAdminsOnline.TabIndex = 0;
@@ -180,16 +161,106 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(20)))), ((int)(((byte)(140)))));
-            this.panel4.Location = new System.Drawing.Point(200, 275);
+            this.panel4.Controls.Add(this.pictureBox5);
+            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(100, 275);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(327, 240);
+            this.panel4.Size = new System.Drawing.Size(562, 240);
             this.panel4.TabIndex = 0;
             // 
             // label1
             // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Website Visits";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(109, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 18.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(341, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 35);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Downloads";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Project.Properties.Resources.Download;
+            this.pictureBox5.Location = new System.Drawing.Point(392, 13);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
             // 
             // pictureBox4
             // 
+            this.pictureBox4.Image = global::Project.Properties.Resources.WebVisits;
+            this.pictureBox4.Location = new System.Drawing.Point(95, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(56, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(83, 15);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(53, 59);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(76, 15);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(56, 59);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(79, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 59);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(400, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 40);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "0";
             // 
             // frmDashboard
             // 
@@ -209,15 +280,18 @@
             this.Load += new System.EventHandler(this.frmDashboard_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
 
         }
 
@@ -236,5 +310,11 @@
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label lblNumAdmins;
         private System.Windows.Forms.Label lblAdminsOnline;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
     }
 }
